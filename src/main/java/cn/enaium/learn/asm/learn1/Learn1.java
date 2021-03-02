5f8896bf9daa169cf616bcd87f75db30072d5c9e
Enaium <32991121+Enaium@users.noreply.github.com>
2021-03-02 13:32:45 +0800

    asm

---
+++ src/main/java/cn/enaium/learn/asm/learn1/Learn1.java
@@ -0,0 +1,19 @@
+package cn.enaium.learn.asm.learn1;
+
+import cn.enaium.learn.asm.Test;
+import jdk.internal.org.objectweb.asm.ClassReader;
+
+import java.io.IOException;
+
+/**
+ * Project: asm
+ * -----------------------------------------------------------
+ * Copyright © 2020-2021 | Enaium | All rights reserved.
+ */
+public class Learn1 {
+    public static void main(String[] args) throws IOException {
+        MyClassVisitor myClassVisitor = new MyClassVisitor();
+        ClassReader classReader = new ClassReader(Test.class.getName());
+        classReader.accept(myClassVisitor, 0);
+    }
+}
---
+++ src/main/java/cn/enaium/learn/asm/learn1/MyClassVisitor.java
@@ -0,0 +1,42 @@
+package cn.enaium.learn.asm.learn1;
+
+import jdk.internal.org.objectweb.asm.ClassVisitor;
+import jdk.internal.org.objectweb.asm.FieldVisitor;
+import jdk.internal.org.objectweb.asm.MethodVisitor;
+
+import static jdk.internal.org.objectweb.asm.Opcodes.ASM5;
+
+/**
+ * Project: asm
+ * -----------------------------------------------------------
+ * Copyright © 2020-2021 | Enaium | All rights reserved.
+ */
+public class MyClassVisitor extends ClassVisitor {
+    public MyClassVisitor() {
+        super(ASM5);
+    }
+
+    @Override
+    public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
+        System.out.println(name + " extends " + superName + " {");
+        super.visit(version, access, name, signature, superName, interfaces);
+    }
+
+    @Override
+    public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
+        System.out.println(desc + " " + name);
+        return super.visitField(access, name, desc, signature, value);
+    }
+
+    @Override
+    public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
+        System.out.println(name + " " + desc);
+        return super.visitMethod(access, name, desc, signature, exceptions);
+    }
+
+    @Override
+    public void visitEnd() {
+        System.out.println("}");
+        super.visitEnd();
+    }
+}
